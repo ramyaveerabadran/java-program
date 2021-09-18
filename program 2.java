4d97974058d087cc3f33aaa05b4213f455a0c9f4
ramyaveerabadran <90092215+ramyaveerabadran@users.noreply.github.com>
2021-09-18 14:45:49 +0530

    Create program 2.java

---
+++ program 2.java
@@ -0,0 +1,11 @@
+//Simple Example of Object and Class
+	public class Student1{  
+	 int id;//data member (also instance variable)  
+	 String name;//data member(also instance variable)  
+	  
+	 public static void main(String args[]){  
+	  Student1 s1=new Student1();//creating an object of Student  
+	  System.out.println(s1.id);  
+	  System.out.println(s1.name);  
+	 }  
+	}  

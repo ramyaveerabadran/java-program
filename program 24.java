0353c9560bf5714b8add7d9a524a4b3f6a5b7479
ramyaveerabadran <90092215+ramyaveerabadran@users.noreply.github.com>
2021-09-18 16:03:59 +0530

    Update program 24.java

--- program 24.java
+++ program 24.java
@@ -14,3 +14,9 @@
 	   int rsquare=op.square(radius);//code reusability (i.e. delegates the method call).  
 	   return pi*rsquare;  
 	 }  
+	 public static void main(String args[]){  
+	   Circle c=new Circle();  
+	   double result=c.area(5);  
+	   System.out.println(result);  
+	 }  
+	}  

88e9b1e883b3f4ba62194934f9dc644e1ca699da
ramyaveerabadran <90092215+ramyaveerabadran@users.noreply.github.com>
2021-09-18 15:51:24 +0530

    Create program 21.java

---
+++ program 21.java
@@ -0,0 +1,13 @@
+//Example of this keyword that you return as a statement from the method
+	public class A{  
+	A getA(){  
+	return this;  
+	}  
+	void msg(){System.out.println("Hello java");}  
+	}  
+	  
+	class Test1{  
+	public static void main(String args[]){  
+	new A().getA().msg();  
+	}  
+	}  

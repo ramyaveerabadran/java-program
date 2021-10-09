2f3d3f1805c50dec5e88e99ba9f980cc7bf79af4
ramyaveerabadran <90092215+ramyaveerabadran@users.noreply.github.com>
2021-10-09 14:56:41 +0530

    Create program 33.java

---
+++ program 33.java
@@ -0,0 +1,11 @@
+//Java final class
+	public final class Bike{}  
+	  
+	class Honda1 extends Bike{  
+	  void run(){System.out.println("running safely with 100kmph");}  
+	    
+	  public static void main(String args[]){  
+	  Honda1 honda= new Honda();  
+	  honda.run();  
+	  }  
+	}  
